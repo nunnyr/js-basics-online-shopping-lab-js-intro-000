708d12fec98d6aa043eb6c8762c015c6a67103e6
Samantha R <nunnyreyes@gmail.com>
2017-12-04 21:22:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,32 +22,32 @@ function addToCart(item) {
 
 
 //
-function viewCart() {
-  let itemsandprices = [];
-  let initial = 'In your cart, you have';
-  for (var i = 0; i < cart.length; i++){
-    //we need to access the key  and the value in the array. It is an array with objects.
-    //Use array method.
-    let itemInArray = Object.keys(cart[i]);
-    let priceInArray = cart[i][itemInArray];
-        itemsandprices.push(itemInArray +' at $'+ priceInArray)  }
-    let twoItems = itemsandprices.join(' and ');
-    let first = itemsandprices.slice(0, -1).join(', ');
-    let last = itemsandprices.slice(-1);
-
-      if (cart.length === 0){
-        console.log('Your shopping cart is empty.')
-      }
-        else if (cart.length === 1){
-          console.log(`${initial} ${itemsandprices}.`)
-        }
-          else if (cart.length === 2){
-            console.log(`${initial} ${twoItems}.`)
-          }
-            else if (cart.length > 2){
-              return(`${initial} ${first}, and ${last}.`)
-            }
-  }
+// function viewCart() {
+//   let itemsandprices = [];
+//   let initial = 'In your cart, you have';
+//   for (var i = 0; i < cart.length; i++){
+//     //we need to access the key  and the value in the array. It is an array with objects.
+//     //Use array method.
+//     let itemInArray = Object.keys(cart[i]);
+//     let priceInArray = cart[i][itemInArray];
+//         itemsandprices.push(itemInArray +' at $'+ priceInArray)  }
+//     let twoItems = itemsandprices.join(' and ');
+//     let first = itemsandprices.slice(0, -1).join(', ');
+//     let last = itemsandprices.slice(-1);
+// 
+//       if (cart.length === 0){
+//         console.log('Your shopping cart is empty.')
+//       }
+//         else if (cart.length === 1){
+//           console.log(`${initial} ${itemsandprices}.`)
+//         }
+//           else if (cart.length === 2){
+//             console.log(`${initial} ${twoItems}.`)
+//           }
+//             else if (cart.length > 2){
+//               return(`${initial} ${first}, and ${last}.`)
+//             }
+  //}
   //
   // function viewCart() {
   // var itemsandprices = [];
@@ -66,7 +66,44 @@ function viewCart() {
   // else if (cart.length > 2){return(`${intro} ${beginning}, and ${end}.`)}
   // }
 
-
+  function viewCart() {
+    getCart();
+    var arr = [];
+    console.log(arr);
+    var keyArr = [];
+    if(Object.keys(getCart()).length > 0){
+    for(var c = 0; c < Object.keys(getCart()).length; c++){
+      arr.push(Object.keys(getCart())[c]);
+    }
+  }
+    for(var a = 0; a < arr.length; a++){
+      keyArr.push(Object.keys(getCart()[a]))
+    }
+    if(keyArr.length < 1){
+      console.log("Your shopping cart is empty.");
+    }
+    if(keyArr.length === 1){
+      console.log(`In your cart, you have ${keyArr[0]} at $${getCart()[0][keyArr[0]]}.`)
+    }
+    if(keyArr.length === 2){
+        var contents2 = "In your cart, you have";
+          for(var p = 0; p < keyArr.length - 1; p++){
+            contents2 += ` ${keyArr[p]} at $${getCart()[p][keyArr[p]]}`;
+          } for(var q = keyArr.length-1; q < keyArr.length; q++){
+              contents2 += ` and ${keyArr[q]} at $${getCart()[p][keyArr[q]]}.`;
+              console.log(contents2);
+          }
+        }
+    if(keyArr.length > 2){
+        var contents = "In your cart, you have";
+          for(var i = 0; i < keyArr.length - 1; i++){
+            contents += ` ${keyArr[i]} at $${getCart()[i][keyArr[i]]},`;
+          } for(var j = keyArr.length-1; j < keyArr.length; j++){
+              contents += ` and ${keyArr[j]} at $${getCart()[i][keyArr[j]]}.`;
+          }
+          console.log(contents);
+    }
+  }
 
 
 
